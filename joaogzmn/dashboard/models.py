c7acd3b0786d1dcd9768c37abb64892002a1fcbb
joaoguzman <joaogg@ug.uchile.cl>
2021-05-25 23:21:00 -0400

    'dashboard_1'

--- joaogzmn/dashboard/models.py
+++ joaogzmn/dashboard/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    empresa = models.CharField(max_length=20)
+    sector = models.CharField(max_length=50)
+    rubro = models.CharField(max_length=50)
+    porcentaje_ddhh = models.DecimalField(max_digits=5, decimal_places=2)
