542c7833cb909d7a031eb5a70f32e5b01c52ecd4
Joao Guzman <joaogg@ug.uchile.cl>
2021-05-26 22:35:09 -0400

    flexmonster

--- joaogzmn/dashboard/urls.py
+++ joaogzmn/dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'dashboard'
+
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.dashboard_with_pivot, name='dashboard_with_pivot'),
     path('data', views.pivot_data, name='pivot_data'),
 ]
